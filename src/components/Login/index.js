ba3da1b061986a7887935af07a6572319251fed0
ArturoGarciaL56 <garcialuejearturo@gmail.com>
2023-02-23 00:24:40 +0100

    CSS Update

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,28 +1,39 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { UserContext } from "../../contexto/UserContext";
 import { login } from "../../services/user";
 
 export const Login = () => {
-    const { loginUser } = useContext(UserContext)
-    const loginNow = async (event) => {
-        try {
-            event.preventDefault()
-            const email = event.target.email.value
-            const password = event.target.password.value
-            const loggedUser = await login({ email, password })
-            loginUser(loggedUser)
-        } catch (e) {
-            alert(e)
-            event.target.reset()
-        }
+  const { loginUser } = useContext(UserContext);
+  const loginNow = async (event) => {
+    try {
+      event.preventDefault();
+      const email = event.target.email.value;
+      const password = event.target.password.value;
+      const loggedUser = await login({ email, password });
+      loginUser(loggedUser);
+    } catch (e) {
+      alert(e);
+      event.target.reset();
     }
-    return (<div>
-        <form onSubmit={loginNow}>
-        <input name="email" type="email" className="form-control" placeholder="Correo electronico" />
-        <input name="password" type="password" className="form-control" placeholder="Contraseña" />
+  };
+  return (
+    <div className="LogInBox">
+      <form onSubmit={loginNow} className="LogInForm">
+        <input
+          name="email"
+          type="email"
+          className="form-control"
+          placeholder="Correo electronico"
+        />
+        <input
+          name="password"
+          type="password"
+          className="form-control"
+          placeholder="Contraseña"
+        />
         <button className="btn btn-primary">Login</button>
-        </form>
-        <a href='/registeruser'>Registrase</a>
-    </div>)
-    
-}
+      </form>
+      <a href="/registeruser">Registrase</a>
+    </div>
+  );
+};
